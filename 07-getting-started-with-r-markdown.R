1f149f16c12aabf94567a51a59c00baabcc40dad
Péter Király <pkiraly@gwdg.de>
2024-11-27 14:58:31 +0100

    Chapter 7.

---
+++ 07-getting-started-with-r-markdown.R
@@ -0,0 +1,17 @@
+#' Getting started with R Markdown
+#' 
+#' Questions
+#' - What is R Markdown?
+#' - How can I integrate my R code with text and plots?
+#' - How can I convert .Rmd files to .html?
+#' 
+#' Objectives
+#' - Create a .Rmd document containing R code, text, and plots
+#' - Create a YAML header to control output
+#' - Understand basic syntax of (R)Markdown
+#' - Customise code chunks to control formatting
+#' - Use code chunks and in-line code to create dynamic, reproducible documents
+
+
+
+  
